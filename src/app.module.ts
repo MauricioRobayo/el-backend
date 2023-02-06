f4cd235becba59f157e0869b3c661c8ea3cf9e0b
Mauricio Robayo <rfmajo@gmail.com>
2023-02-06 16:30:21 -0500

    2/mongodb setup (#5)
    * feat(#2): add docker-compose.yml
    
    * fix(#2): mongodb setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MoviesModule, ConfigModule.forRoot()],
+  imports: [
+    MoviesModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
