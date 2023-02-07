39dd407eabbc4e5933791d3aad71c2e2bc3299ff
Mauricio Robayo <rfmajo@gmail.com>
2023-02-06 20:12:29 -0500

    fix(#6): update movie Entity according to requirements (#7)
    closes #6

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -3,18 +3,6 @@ export interface Genre {
   name: string;
 }
 
-export interface ProductionCompany {
-  id: number;
-  logo_path: string;
-  name: string;
-  origin_country: string;
-}
-
-export interface ProductionCountry {
-  iso_3166_1: string;
-  name: string;
-}
-
 export interface SpokenLanguage {
   english_name: string;
   iso_639_1: string;
@@ -22,29 +10,17 @@ export interface SpokenLanguage {
 }
 
 export class Movie {
-  adult: boolean;
-  backdrop_path: string;
-  belongs_to_collection?: any;
-  budget: number;
   genres: Genre[];
-  homepage: string;
-  id: number;
-  imdb_id: string;
-  original_language: string;
-  original_title: string;
+  language: SpokenLanguage[];
+  movieApiId: number;
+  movieIMDBId: string;
+  originalLanguage: string;
   overview: string;
   popularity: number;
-  poster_path?: any;
-  production_companies: ProductionCompany[];
-  production_countries: ProductionCountry[];
-  release_date: string;
-  revenue: number;
-  runtime: number;
-  spoken_languages: SpokenLanguage[];
-  status: string;
-  tagline: string;
+  posterPath: string;
+  releaseDate: string;
   title: string;
   video: boolean;
-  vote_average: number;
-  vote_count: number;
+  voteAverage: number;
+  voteCount: number;
 }
---
+++ src/movies/movie-api/tmdb-api/trmdb-movie.mapper.ts
@@ -0,0 +1,54 @@
+import { Genre, Movie, SpokenLanguage } from '../../entities/movie.entity';
+
+export interface TmdbResult {
+  page: number;
+  results: TmdbMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface TmdbMovie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection?: any;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path?: any;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export class TmdbMovieMapper {
+  mapToMovieEntity(tmdbMovie: TmdbMovie): Movie {
+    return {
+      genres: tmdbMovie.genres,
+      language: tmdbMovie.spoken_languages,
+      movieApiId: tmdbMovie.id,
+      movieIMDBId: tmdbMovie.imdb_id,
+      originalLanguage: tmdbMovie.original_language,
+      overview: tmdbMovie.overview,
+      popularity: tmdbMovie.popularity,
+      posterPath: tmdbMovie.poster_path,
+      releaseDate: tmdbMovie.release_date,
+      title: tmdbMovie.title,
+      video: tmdbMovie.video,
+      voteAverage: tmdbMovie.vote_average,
+      voteCount: tmdbMovie.vote_count,
+    };
+  }
+}
