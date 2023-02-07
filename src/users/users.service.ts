487a87c6e85dde9cec6ec024f9f219bba3f6ae0b
Mauricio Robayo <rfmajo@gmail.com>
2023-02-07 12:53:17 -0500

    12/create-user-mock-endpoint (#13)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateFavoriteDto } from './dto/create-favorite.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserDto } from './dto/user.dto';
 import { User } from './entities/user.entity';
 import { UserMapper } from './users.mapper';
@@ -12,6 +13,12 @@ export class UsersService {
     @InjectModel(User.name) private readonly usersModel: Model<User>,
     private readonly userMapper: UserMapper,
   ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<UserDto> {
+    const newUser = new this.usersModel(createUserDto);
+    const createdUser = await newUser.save();
+    return this.userMapper.mapToUserDto(createdUser);
+  }
 
   async createFavorite({
     userId,
