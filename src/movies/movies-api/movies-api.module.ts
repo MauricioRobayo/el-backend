63920311d379b8724f7c0cc3b84e946356e8bbfb
Mauricio Robayo <rfmajo@gmail.com>
2023-02-07 09:06:30 -0500

    chore(#8): improve comments

--- src/movies/movies-api/movies-api.module.ts
+++ src/movies/movies-api/movies-api.module.ts
@@ -1,16 +1,16 @@
 import { Module, DynamicModule } from '@nestjs/common';
 import { TmdbApiModule } from './tmdb-api/tmdb-api.module';
 
-type MoviesApiName = 'tmdb'; // 'tmdb' | 'otherMovieApi' | 'anotherOne'
+type MoviesApiName = 'tmdb'; // 'tmdb' | 'otherMoviesApi' | 'anotherOne'
 
 @Module({})
 export class MoviesApiModule {
   static forRoot(moviesApiName?: MoviesApiName): DynamicModule {
     switch (moviesApiName) {
-      // case 'otherMovieApi':
-      //   return {/* ... */}
+      // case 'otherMoviesApi':
+      //   return { module: OtherMoviesApi, /* ... */ }
       // case 'anotherOne':
-      //   return {/* ... */}
+      //   return { module: AnotherOne, /* ... */}
       //
       //
       // default to 'tmdb' if no argument provided
