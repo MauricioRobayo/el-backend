0ca9ce5e23a3fde52f1f92e06717477b2c5adcea
Mauricio Robayo <rfmajo@gmail.com>
2023-02-08 12:41:28 -0500

    test(#29): add missing providers

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,8 @@
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TmdbApiModule } from '../common/movies-api/tmdb-api/tmdb-api.module';
+import { TmdbApiService } from '../common/movies-api/tmdb-api/tmdb-api.service';
+import { TmdbMovieMapper } from '../common/movies-api/tmdb-api/trmdb-movie.mapper';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -6,7 +10,8 @@ describe('MoviesService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MoviesService],
+      imports: [TmdbApiModule, HttpModule],
+      providers: [MoviesService, TmdbMovieMapper],
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
