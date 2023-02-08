d6a0f664862cc4ee9aec27553e3eec48af56acd3
Mauricio Robayo <rfmajo@gmail.com>
2023-02-07 21:12:18 -0500

    fix(#25): add imageUrl to note

--- src/users/dto/create-note.dto.ts
+++ src/users/dto/create-note.dto.ts
@@ -1,9 +1,14 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString, IsUrl } from 'class-validator';
 
 export class CreateNoteDto {
   @IsString()
   readonly title: string;
 
+  @IsOptional()
   @IsString()
   readonly description: string;
+
+  @IsOptional()
+  @IsUrl()
+  readonly imageUrl: string;
 }
