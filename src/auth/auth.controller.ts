30050b8c70df47214c4815a49f6f24890adf0e1c
Mauricio Robayo <rfmajo@gmail.com>
2023-02-12 21:09:38 -0500

    chore(#40): add return typings

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,19 +8,24 @@ import {
 import { AwsCognitoService } from './providers/aws-cognito.service';
 import { AuthLoginUserDto } from './dto/auth-login-user.dto';
 import { AuthRegisterUserDto } from './dto/auth-register-user.dto';
+import { AuthenticatedUserDto } from './dto/authenticated-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private awsCognitoService: AwsCognitoService) {}
 
   @Post('/register')
-  async register(@Body() authRegisterUserDto: AuthRegisterUserDto) {
+  async register(
+    @Body() authRegisterUserDto: AuthRegisterUserDto,
+  ): Promise<void> {
     return await this.awsCognitoService.registerUser(authRegisterUserDto);
   }
 
   @Post('/login')
   @UsePipes(ValidationPipe)
-  async login(@Body() authLoginUserDto: AuthLoginUserDto) {
+  async login(
+    @Body() authLoginUserDto: AuthLoginUserDto,
+  ): Promise<AuthenticatedUserDto> {
     return await this.awsCognitoService.authenticateUser(authLoginUserDto);
   }
 }
